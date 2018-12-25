83664eb0a0de318159d5b59a605f6301fb39ce69
Andras Belicza <iczaaa@gmail.com>
2018-12-25 18:42:39 +0100

    Implement Weights() and WeightsFold(), add tests

--- abcsort.go
+++ abcsort.go
@@ -5,18 +5,36 @@ Package abcsort is a sorting library that uses a custom alphabet.
 */
 package abcsort
 
+import (
+	"strings"
+)
+
 // Weights returns a map containing runes of the given alphabet, position of
 // each rune is used as the weight.
 func Weights(alphabet string) map[rune]int {
-	// TODO
-	return nil
+	weights := map[rune]int{}
+	addWeights(alphabet, weights)
+	return weights
 }
 
 // WeightsFold returns a map containing lower and upper versions of all runes
 // of the given alphabet, position of each rune is used as the weight.
 func WeightsFold(alphabet string) map[rune]int {
-	// TODO
-	return nil
+	weights := map[rune]int{}
+	addWeights(strings.ToLower(alphabet), weights)
+	addWeights(strings.ToUpper(alphabet), weights)
+	return weights
+}
+
+// addWeights uses the given map to add the weights to.
+func addWeights(alphabet string, weights map[rune]int) {
+	for i, r := range alphabet {
+		// NOTE: i is the byte index (not the rune index), but since it is also
+		// monotone increasing, it will just just as fine for rune weight.
+		if r != 0xfffd {
+			weights[r] = i
+		}
+	}
 }
 
 // Less tells if s1 is less than s2, interpreted as UTF-8 strings,
---
+++ abcsort_test.go
@@ -0,0 +1,46 @@
+package abcsort
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestWeights_WeightsFold(t *testing.T) {
+	cases := []struct {
+		title       string
+		alphabet    string
+		weights     map[rune]int
+		weightsFold map[rune]int
+	}{
+		{
+			title:       "empty",
+			alphabet:    "",
+			weights:     map[rune]int{},
+			weightsFold: map[rune]int{},
+		},
+		{
+			title:       "normal",
+			alphabet:    "abc",
+			weights:     map[rune]int{'a': 0, 'b': 1, 'c': 2},
+			weightsFold: map[rune]int{'a': 0, 'b': 1, 'c': 2, 'A': 0, 'B': 1, 'C': 2},
+		},
+		{
+			title:       "invalid",
+			alphabet:    "\xfa\xfb\xfc",
+			weights:     map[rune]int{},
+			weightsFold: map[rune]int{},
+		},
+	}
+
+	for _, c := range cases {
+		weights := Weights(c.alphabet)
+		if !reflect.DeepEqual(weights, c.weights) {
+			t.Errorf("[%s] Expected weights: %v, got: %v", c.title, c.weights, weights)
+		}
+
+		weightsFold := WeightsFold(c.alphabet)
+		if !reflect.DeepEqual(weightsFold, c.weightsFold) {
+			t.Errorf("[%s] Expected weights fold: %v, got: %v", c.title, c.weightsFold, weightsFold)
+		}
+	}
+}
